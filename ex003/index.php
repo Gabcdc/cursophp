5c2c07665bae79a25f4832b60617da386415e234
gabriel234239094 <130863137+Gabcdc@users.noreply.github.com>
2024-01-09 13:37:02 -0300

    Initial commit

---
+++ ex003/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos de php</title>
+</head>
+<body>
+    <h1>Testestes de tipos primitivos</h1>
+    <?php 
+    // $num = 010;
+    // echo "O valor da variável é $num "
+
+
+    // $v = 300;
+    // var_dump($v);
+
+
+    $num =3e2;
+    echo "O valor é $num";
+    var_dump($num);
+    
+    ?>
+    
+</body>
+</html>
